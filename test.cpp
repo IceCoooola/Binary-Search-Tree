0b091a466f683b3cfd2c10b03e9055c661f49391
Ice cola <90940182+IceCoooola@users.noreply.github.com>
2022-09-06 00:31:25 -0700

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -8,18 +8,43 @@ int main()
 	t.Add(4);
 	t.InOrder();
 	cout << endl;
+	Node<int>* tt = t.find(2);
+	if (tt)
+	{
+		cout << tt->_data<< " found\n";
+	}
 	t.Add(1);
 	t.InOrder();
 	cout << endl;
 	t.Add(3);
 	t.InOrder();
 	cout << endl;
+	Node<int>* tt2 = t.find(3);
+	if (tt2)
+	{
+		cout << tt2->_data << " found\n";
+	}
 	t.Add(2);
 	t.InOrder();
 	cout << endl;
 	t.Add(6);
 	t.InOrder();
 	cout << endl;
+	t.erase(2);
+	t.InOrder();
+	cout << endl;
+	t.erase(3);
+	t.InOrder();
+	cout << endl;
+	t.erase(1);
+	t.InOrder();
+	cout << endl;
+	t.erase(4);
+	t.InOrder();
+	cout << endl;
+	t.erase(6);
+	t.InOrder();
+	cout << endl;
+
 	return 0;
 }
-
